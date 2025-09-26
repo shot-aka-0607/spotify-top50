0e36a406b4dd9f534d75ad4389c37c7d132160c0
shot-aka-0607 <49662073+shot-aka-0607@users.noreply.github.com>
2025-09-27 03:38:13 +0900

    Add files via upload

---
+++ kawaiilab_hobby15.py
@@ -0,0 +1,31 @@
+import os
+from spotipy import Spotify
+from spotipy.oauth2 import SpotifyOAuth
+
+# 認証情報を環境変数から取得
+CLIENT_ID = os.environ["SPOTIPY_CLIENT_ID"]
+CLIENT_SECRET = os.environ["SPOTIPY_CLIENT_SECRET"]
+REDIRECT_URI = os.environ.get("SPOTIPY_REDIRECT_URI", "http://localhost:8888/callback")
+REFRESH_TOKEN = os.environ["SPOTIPY_REFRESH_TOKEN"]
+
+SCOPE = "playlist-modify-public playlist-read-private"
+
+# 非対話で refresh_token を使って認証
+sp_oauth = SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET,
+                        redirect_uri=REDIRECT_URI, scope=SCOPE)
+token_info = sp_oauth.refresh_access_token(REFRESH_TOKEN)
+sp = Spotify(auth=token_info["access_token"])
+
+# --- 上位20曲を抽出したいプレイリスト ---
+source_playlist_id = "https://open.spotify.com/playlist/07kXPcjIqWw5DUT4Ybsdod?si=1c40037b7ad84e47"  # 置き換え
+target_playlist_id = "https://open.spotify.com/playlist/1anCYH7pLqOzgW7F53Kn0M?si=cefc6607fdb94ef1"  # 置き換え
+
+# 1. 上位20曲取得
+results = sp.playlist_items(source_playlist_id, limit=15)
+track_uris = [item["track"]["uri"] for item in results["items"]]
+
+# 2. 別プレイリストに上書き
+sp.playlist_replace_items(target_playlist_id, track_uris)
+
+print(f"プレイリスト「KAWAII LAB. Top15」を更新しました（{len(track_uris)}曲）")
+
